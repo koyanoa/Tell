23582da730f5cd827393151a2d123eb0047c6b89
bbinoy <beb@posteo.de>
2015-06-03 14:43:30 +0200

    now using post instead of get request

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var app = require('express')();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 var port = process.env.PORT || 5000;
 
 // create a server with the express app as a listener
@@ -11,53 +13,51 @@ var BinaryServer = require('binaryjs').BinaryServer;
 var bs = new BinaryServer({server: server, path: '/binary-endpoint'});
 console.log('BinaryServer running');
 
-app.get('/start', function(req, res){
-	id = getID();
-	console.log('ID ' + id + ' requested');
-	res.send(id);
-});
-
-app.get('/join', function(req, res){
-	console.log('User wants to connect to ' + id);
-	res.send('Trying to connect you');
+app.post('/action', function(req, res){
+	var action = req.body.action;
+	console.log('--------- New ' + action + ' requested ----------');
+	if (action == 'start'){
+		id = getID();
+		console.log('User requests ' + id);
+		res.send(id);
+	}
+	else if (action == 'join'){
+		join_id = req.body.id;
+		console.log('User wants to connect to ' + join_id);
+		res.send('Trying to connect you. The ID you send is ' + join_id);
+	}
+	else console.log('Problem recognizing action');
 });
 
 // Create 6 digit session IDs
 var id_free = [], // all available IDs
 	id_used = []; // all currently available IDs
 
-for(i=0;i<1000000;i++){
+// Generate array with all IDs and shuffle it
+for(var i=0;i<1000000;i++){
 	id_free.push(('00000' + i.toString()).slice(-6));
 }
+function shuffle(o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+}
+shuffle(id_free);
 
+// get a random ID from free ID array
 function getID(){
 	randindex = Math.round(Math.random()*1000000);
-	id = id_free[randindex]
+	var id = id_free[randindex]
 	id_used.push(id);
 	id_free.splice(randindex,1);
 	return id;
 }
 
+// make ID available for use again
 function freeID(id){
 	id_free.push(id); // add to array of free IDs
 	id_used.splice(id.indexOf(),1); // remove from array of used IDs
 	return;
 }
-
-// Test getID and freeID functions
-/*for(i=0;i<10;i++){
-	console.log('Round' + i);
-	id = getID();
-	console.log(id_used);
-	console.log('Length of free ID array: ' + id_free.length);
-}
-
-for(i=0;i<10;i++){
-	freeID(id_used[0]);
-	console.log(id_used);
-	console.log('Length of free ID array: ' + id_free.length);
-	console.log(id_used[id_used.length-1]);
-}*/
 
 // wait for new user connections
 bs.on('connection', function(client) {
