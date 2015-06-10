ca563cdda904f642d8796207a913271db8d25618
Timo Horstschäfer <timoho@public-docking-hpx-2502.ethz.ch>
2015-06-10 18:33:14 +0200

    Add https to echo

--- app/echo.js
+++ app/echo.js
@@ -1,18 +1,29 @@
-var http = require('http');
+var fs = require('fs');
+var https = require('https');
 var express = require('express');
-var app = express();
-var port = process.env.PORT || 5000;
 
-// create a server with the express app as a listener
-var server = http.createServer(app)
+var privateKey  = fs.readFileSync('tls/ssl.dec.key', 'utf8');
+var certificate = fs.readFileSync('tls/ssl.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+var app = express();
+var port = process.env.PORT || 62938;
+var log = false;
+
+
+//... bunch of other express stuff here ...
+
+//pass in your express app and credentials to create an https server
+var server = https.createServer(credentials, app);
 console.log('HTTP server running on port ' + port);
 
-// make subdirectory public available on localhost to call files
+// make subdirectory public available on server to call files
 app.use(express.static(__dirname + '/public'));
 
 // attach BinaryServer to the base http server
 var BinaryServer = require('binaryjs').BinaryServer;
-var bs = BinaryServer({server: server, path:'/binary-endpoint'});
+var bs = new BinaryServer({server: server});
+var noFile = new Buffer(0); // for sending only metadata through binaryjs
 console.log('BinaryServer running');
 
 
